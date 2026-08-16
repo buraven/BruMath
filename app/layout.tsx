f8419c7aee195db06b8c4636a22a8ed820832589
Bru Oliveira <brunaoliemp@gmail.com>
2026-08-16 17:59:06 -0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,1 @@
+import "./globals.css";export const metadata={title:"BruMath",description:"Finanças de Bruna e Matheus"};export default function Layout({children}:{children:React.ReactNode}){return <html lang="pt-BR"><body>{children}</body></html>}
